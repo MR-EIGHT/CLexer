65d58f75ed96adb96cf234b55f31159f84c00cf1
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-17 03:24:40 +0330

    basic operators & numbers covered. c code edited.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Token import Token
 
 Tokens = []
-keywords = ['int', 'char', 'printf']
+keywords = ['int', 'char', 'printf', 'scanf']
 
 
 def add_token(Type, literal, col, row, blockno):
@@ -15,7 +15,7 @@ row = 0
 block_no = 0
 index = -1
 
-while index < len(code):
+while index < len(code)-1:
     index += 1
     current_char = code[index]
     if current_char.isspace():
@@ -63,6 +63,52 @@ while index < len(code):
         case ')':
             col += 1
             add_token('operator', code[index], col, row, block_no)
+
+        case ';':
+            col += 1
+            add_token('semi-colon', code[index], col, row, block_no)
+
+        case '=':
+            if code[index + 1] == '=':
+                col += 2
+                add_token('boolean operator', code[index], col, row, block_no)
+            else:
+                col += 1
+                add_token('operator', code[index], col, row, block_no)
+
+        case current_char if current_char.isnumeric():
+            c_index = index
+            while code[index].isnumeric():
+                index += 1
+                col += 1
+            add_token('number', code[c_index:index], col, row, block_no)
+            index -= 1
+
+        case '"':
+            c_index = index
+            index += 1
+            col += 1
+            while code[index] != '"':
+                index += 1
+                col += 1
+            add_token('string', code[c_index:index + 1], col, row, block_no)
+
+        case ',':
+            col += 1
+            add_token('comma', code[index], col, row, block_no)
+
+        case '&':
+            if code[index + 1] == '=':
+                col += 2
+                add_token('operator', code[index:index + 2], col, row, block_no)
+
+            elif code[index + 1] == '&':
+                col += 2
+                add_token('boolean operator', code[index:index + 2], col, row, block_no)
+
+            else:
+                col += 1
+                add_token('ampersand', code[index], col, row, block_no)
 
         case _:
             c_index = index
