7728fdabe20751cf1fe7ce272d0771ef7d9a7877
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-17 02:52:51 +0330

    some operators added.

--- main.py
+++ main.py
@@ -1,5 +1,79 @@
-code = open("hello.c", mode='r')
+from Token import Token
 
-for line_no, line in enumerate(code.readlines()):
-    if line.strip() != "":
-        print(str(line_no) + " " + line)
+Tokens = []
+keywords = ['int', 'char', 'printf']
+
+
+def add_token(Type, literal, col, row, blockno):
+    Tokens.append(Token(Type, literal, row, col, blockno))
+
+
+code = open("hello.c", mode='r').read()
+
+col = 0
+row = 0
+block_no = 0
+index = -1
+
+while index < len(code):
+    index += 1
+    current_char = code[index]
+    if current_char.isspace():
+        match current_char:
+            case '\t':
+                col += 4
+            case ' ':
+                col += 1
+            case '\n':
+                col = 0
+                row += 1
+        continue
+
+    match current_char:
+        case '+':
+            if code[index + 1] == '=':
+                col += 2
+                add_token('operator', code[index:index + 2], col, row, block_no)
+
+            elif code[index + 1] == '+':
+                col += 2
+                add_token('operator', code[index:index + 2], col, row, block_no)
+
+            else:
+                col += 1
+                add_token('operator', code[index], col, row, block_no)
+
+        case '{':
+            col += 1
+            block_no += 1
+            add_token('operator', code[index], col, row, block_no)
+
+        case '}':
+            col += 1
+            block_no -= 1
+            add_token('operator', code[index], col, row, block_no)
+
+        case '#':
+            continue
+
+        case '(':
+            col += 1
+            add_token('operator', code[index], col, row, block_no)
+
+        case ')':
+            col += 1
+            add_token('operator', code[index], col, row, block_no)
+
+        case _:
+            c_index = index
+            while code[index].isalnum():
+                index += 1
+                col += 1
+
+            if code[c_index:index] in keywords:
+                add_token('keyword', code[c_index:index], col, row, block_no)
+            else:
+                add_token('identifier', code[c_index:index], col, row, block_no)
+            index -= 1
+
+print("hi")
