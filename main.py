a275368f311a6b710341de30a719784b9c05b948
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-17 04:36:14 +0330

    critical bug fixed! some operators added.

--- main.py
+++ main.py
@@ -34,10 +34,12 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             elif code[index + 1] == '+':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -47,10 +49,12 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             elif code[index + 1] == '-':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -60,6 +64,7 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -69,6 +74,7 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             elif code[index + 1] == '/':
                 while code[index] != '\n':
@@ -85,14 +91,36 @@ while index < len(code) - 1:
                 col += 1
                 add_token('operator', code[index], col, row, block_no)
 
+        case '%':
+            if code[index + 1] == '=':
+                col += 2
+                add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
+
+            else:
+                col += 1
+                add_token('percent', code[index], col, row, block_no)
+
+        case '!':
+            if code[index + 1] == '=':
+                col += 2
+                add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                index += 1
+
+            else:
+                col += 1
+                add_token('bit-wise operator', code[index], col, row, block_no)
+
         case '|':
             if code[index + 1] == '=':
                 col += 2
                 add_token('bit-wise operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             elif code[index + 1] == '|':
                 col += 2
                 add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -102,10 +130,12 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             elif code[index + 1] == '&':
                 col += 2
                 add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -115,6 +145,7 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('operator', code[index:index + 2], col, row, block_no)
+                index += 1
 
             else:
                 col += 1
@@ -178,6 +209,7 @@ while index < len(code) - 1:
             if code[index + 1] == '=':
                 col += 2
                 add_token('boolean operator', code[index], col, row, block_no)
+                index += 1
             else:
                 col += 1
                 add_token('operator', code[index], col, row, block_no)
@@ -203,7 +235,36 @@ while index < len(code) - 1:
             col += 1
             add_token('comma', code[index], col, row, block_no)
 
-        case _:
+        case '<':
+            if code[index + 1] == '<':
+                col += 2
+                add_token('left shift', code[index:index + 2], col, row, block_no)
+                index += 1
+
+            elif code[index + 1] == '=':
+                col += 2
+                add_token('relational operator', code[index:index + 2], col, row, block_no)
+                index += 1
+
+            else:
+                col += 1
+                add_token('relational operator', code[index], col, row, block_no)
+
+        case '>':
+            if code[index + 1] == '>':
+                col += 2
+                add_token('right shift', code[index:index + 2], col, row, block_no)
+                index+=1
+
+            elif code[index + 1] == '=':
+                col += 2
+                add_token('relational operator', code[index:index + 2], col, row, block_no)
+                index += 1
+            else:
+                col += 1
+                add_token('relational operator', code[index], col, row, block_no)
+
+        case current_char  if code[index].isalnum():
             c_index = index
             while code[index].isalnum():
                 index += 1
@@ -214,6 +275,11 @@ while index < len(code) - 1:
             else:
                 add_token('identifier', code[c_index:index], col, row, block_no)
             index -= 1
+
+        case _:
+            raise Exception('Error! Unknown character is used.')
+
+
 
 for t in Tokens:
     print(t)
