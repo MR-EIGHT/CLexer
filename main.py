5f2838e293e02ca9894407d543810e54a65ebdfe
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-22 13:12:00 +0330

    index begining of the token as column. not the end.

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def add_token(token_type, token_literal, token_column, token_row, token_block):
 
 code = open("hello.c", mode='r').read()
 
-col = 0
-row = 0
+col = 1
+row = 1
 block_no = 0
 index = -1
 
@@ -29,7 +29,7 @@ while index < len(code) - 1:
             case ' ':
                 col += 1
             case '\n':
-                col = 0
+                col = 1
                 row += 1
         continue
 
@@ -37,52 +37,52 @@ while index < len(code) - 1:
         case '+':
             if code[index + 1] == '=':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '+':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('operator', code[index], col, row, block_no)
+                add_token('operator', code[index], col - 1, row, block_no)
 
         case '-':
             if code[index + 1] == '=':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '-':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '>':
                 col += 2
-                add_token('arrow', code[index:index + 2], col, row, block_no)
+                add_token('arrow', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('operator', code[index], col, row, block_no)
+                add_token('operator', code[index], col - 1, row, block_no)
 
         case '*':
             if code[index + 1] == '=':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('asterisk', code[index], col, row, block_no)
+                add_token('asterisk', code[index], col - 1, row, block_no)
 
         case '/':
             if code[index + 1] == '=':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '/':
@@ -98,77 +98,77 @@ while index < len(code) - 1:
                 index += 1
             else:
                 col += 1
-                add_token('operator', code[index], col, row, block_no)
+                add_token('operator', code[index], col - 1, row, block_no)
 
         case '%':
             if code[index + 1] == '=':
                 col += 2
-                add_token('operator', code[index:index + 2], col, row, block_no)
+                add_token('operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('percent', code[index], col, row, block_no)
+                add_token('percent', code[index], col - 1, row, block_no)
 
         case '!':
             if code[index + 1] == '=':
                 col += 2
-                add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                add_token('boolean operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('bit-wise operator', code[index], col, row, block_no)
+                add_token('bit-wise operator', code[index], col - 1, row, block_no)
 
         case '|':
             if code[index + 1] == '=':
                 col += 2
-                add_token('bit-wise operator', code[index:index + 2], col, row, block_no)
+                add_token('bit-wise operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '|':
                 col += 2
-                add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                add_token('boolean operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('operator', code[index], col, row, block_no)
+                add_token('operator', code[index], col - 1, row, block_no)
 
         case '&':
             if code[index + 1] == '=':
                 col += 2
-                add_token('bit-and assign', code[index:index + 2], col, row, block_no)
+                add_token('bit-and assign', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '&':
                 col += 2
-                add_token('boolean operator', code[index:index + 2], col, row, block_no)
+                add_token('boolean operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('ampersand', code[index], col, row, block_no)
+                add_token('ampersand', code[index], col - 1, row, block_no)
 
         case '^':
             if code[index + 1] == '=':
                 col += 2
-                add_token('xor assign', code[index:index + 2], col, row, block_no)
+                add_token('xor assign', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('xor', code[index], col, row, block_no)
+                add_token('xor', code[index], col - 1, row, block_no)
 
         case '{':
             col += 1
             block_no += 1
-            add_token('left curly-brace', code[index], col, row, block_no)
+            add_token('left curly-brace', code[index], col - 1, row, block_no)
 
         case '}':
             col += 1
             block_no -= 1
-            add_token('right curly-brace', code[index], col, row, block_no)
+            add_token('right curly-brace', code[index], col - 1, row, block_no)
 
         case '#':
             continue
@@ -176,86 +176,93 @@ while index < len(code) - 1:
         case '~':
             if code[index + 1] == '=':
                 col += 2
-                add_token('bit-not assign', code[index:index + 2], col, row, block_no)
+                add_token('bit-not assign', code[index:index + 2], col - 2, row, block_no)
                 index += 1
             else:
                 col += 1
-                add_token('tilda', code[index], col, row, block_no)
+                add_token('tilda', code[index], col - 1, row, block_no)
 
         case '?':
             col += 1
-            add_token('question', code[index], col, row, block_no)
+            add_token('question', code[index], col - 1, row, block_no)
 
         case ':':
             col += 1
-            add_token('colon', code[index], col, row, block_no)
+            add_token('colon', code[index], col - 1, row, block_no)
 
         case '.':
             if code[index: index + 3] == '...':
                 col += 3
-                add_token('ellipsis', code[index:index + 3], col, row, block_no)
+                add_token('ellipsis', code[index:index + 3], col - 3, row, block_no)
                 index += 2
             else:
                 col += 1
-                add_token('colon', code[index], col, row, block_no)
+                add_token('colon', code[index], col - 1, row, block_no)
 
         case '[':
             col += 1
-            add_token('left bracket', code[index], col, row, block_no)
+            add_token('left bracket', code[index], col - 1, row, block_no)
 
         case ']':
             col += 1
-            add_token('right bracket', code[index], col, row, block_no)
+            add_token('right bracket', code[index], col - 1, row, block_no)
 
         case ':':
             col += 1
-            add_token('colon', code[index], col, row, block_no)
+            add_token('colon', code[index], col - 1, row, block_no)
 
         case "'":
-            col += 3
-            add_token('quote', code[index:index + 3], col, row, block_no)
-            index += 3
+            if code[index + 1] == '\'':
+                col += 2
+                add_token('empty-quote', code[index:index + 2], col - 2, row, block_no)
+                index += 1
+            else:
+                col += 3
+                add_token('quote', code[index:index + 3], col - 3, row, block_no)
+                index += 3
 
         case '(':
             col += 1
-            add_token('left parentheses', code[index], col, row, block_no)
+            add_token('left parentheses', code[index], col - 1, row, block_no)
 
         case ')':
             col += 1
-            add_token('right parentheses', code[index], col, row, block_no)
+            add_token('right parentheses', code[index], col - 1, row, block_no)
 
         case ';':
             col += 1
-            add_token('semi-colon', code[index], col, row, block_no)
+            add_token('semi-colon', code[index], col - 1, row, block_no)
 
         case '=':
             if code[index + 1] == '=':
                 col += 2
-                add_token('boolean operator', code[index], col, row, block_no)
+                add_token('boolean operator', code[index], col - 2, row, block_no)
                 index += 1
             else:
                 col += 1
-                add_token('operator', code[index], col, row, block_no)
+                add_token('operator', code[index], col - 1, row, block_no)
 
         case current_char if current_char.isnumeric():
             c_index = index
+            current_col = col
             while code[index].isnumeric() or (code[index] == '.' and code[index + 1].isnumeric()):
                 index += 1
                 col += 1
             if '.' in code[c_index:index]:
-                add_token('float', code[c_index:index], col, row, block_no)
+                add_token('float', code[c_index:index], current_col, row, block_no)
             else:
-                add_token('integer', code[c_index:index], col, row, block_no)
+                add_token('integer', code[c_index:index], current_col, row, block_no)
             index -= 1
 
         case '"':
             c_index = index
+            current_col = col
             index += 1
             col += 1
             while code[index] != '"':
                 index += 1
                 col += 1
-            add_token('string', code[c_index:index + 1], col, row, block_no)
+            add_token('string', code[c_index:index + 1], current_col, row, block_no)
 
         case ',':
             col += 1
@@ -264,51 +271,52 @@ while index < len(code) - 1:
         case '<':
             if code[index: index + 3] == '<<=':
                 col += 3
-                add_token('left-shift assign', code[index:index + 3], col, row, block_no)
+                add_token('left-shift assign', code[index:index + 3], col - 3, row, block_no)
                 index += 2
             elif code[index + 1] == '<':
                 col += 2
-                add_token('left shift', code[index:index + 2], col, row, block_no)
+                add_token('left shift', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '=':
                 col += 2
-                add_token('relational operator', code[index:index + 2], col, row, block_no)
+                add_token('relational operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             else:
                 col += 1
-                add_token('relational operator', code[index], col, row, block_no)
+                add_token('relational operator', code[index], col - 1, row, block_no)
 
         case '>':
             if code[index: index + 3] == '>>=':
                 col += 3
-                add_token('right-shift assign', code[index:index + 3], col, row, block_no)
+                add_token('right-shift assign', code[index:index + 3], col - 3, row, block_no)
                 index += 2
 
             elif code[index + 1] == '>':
                 col += 2
-                add_token('right shift', code[index:index + 2], col, row, block_no)
+                add_token('right shift', code[index:index + 2], col - 2, row, block_no)
                 index += 1
 
             elif code[index + 1] == '=':
                 col += 2
-                add_token('relational operator', code[index:index + 2], col, row, block_no)
+                add_token('relational operator', code[index:index + 2], col - 2, row, block_no)
                 index += 1
             else:
                 col += 1
-                add_token('relational operator', code[index], col, row, block_no)
+                add_token('relational operator', code[index], col - 1, row, block_no)
 
         case current_char if code[index].isalpha() or code[index] == '_':
             c_index = index
+            current_col = col
             while code[index].isalnum() or code[index] == '_':
                 index += 1
                 col += 1
 
             if code[c_index:index] in keywords:
-                add_token('keyword', code[c_index:index], col, row, block_no)
+                add_token('keyword', code[c_index:index], current_col, row, block_no)
             else:
-                add_token('identifier', code[c_index:index], col, row, block_no)
+                add_token('identifier', code[c_index:index], current_col, row, block_no)
             index -= 1
 
         case _:
